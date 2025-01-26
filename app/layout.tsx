42624e7e83afe0607c459adf040ffe49a436c661
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-27 00:39:09 +0530

    some skelenton arrived

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,21 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body
-        className={`antialiased h-full bg-gradient-to-tr from-indigo-600 via-purple-800 to-pink-900 `}
-      >
+      <body className={`h-full bg-gradient-to-tr from-white to-gray-300 pl-10`}>
         {children}
       </body>
     </html>
