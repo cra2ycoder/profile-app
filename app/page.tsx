52a58e0f8c3647b9811847ea972bd6d0557f6bbf
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-26 22:03:49 +0530

    initial setup is done and base code started

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+// import Image from 'next/image'
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="w-5xl h-screen max-w-5xl m-auto px-10">
+      <div className="flex justify-center flex-col gap-4 w-100">
+        <div className="size-25 rounded-full overflow-hidden">
+          {/* <Image
+            src="https://cra2ycoder.sirv.com/mohaa.jpg"
+            width="100"
+            height="100"
+            alt="profile-image"
+          /> */}
+        </div>
+        <h1 className="text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r">
+          Mohanraj Rangasamy
+        </h1>
+        <p className="text-2xl">
+          Innovation Front End Developer with 7+ years experience building and
+          maintaining responsive websites in the recruiting industry.
+          Well-versed in HTML, CSS, Javascript/Typescript, React.js, Next.js;
+          plus modern libraries and frameworks. Passionate about usability and
+          possess working knowledge of Adobe Photoshop & XD.
+        </p>
+      </div>
+    </div>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'Arial', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
