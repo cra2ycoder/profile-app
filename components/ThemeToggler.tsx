0dde93602b0d58fb14b954b32b30fb3467824acf
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-28 19:57:20 +0530

    theme toggle button introduced

---
+++ components/ThemeToggler.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useState } from 'react'
+import { SunOutlinedIcon, SunFilledIcon } from './BGIcons'
+
+export default function ThemeToggler() {
+  const [showLightTheme, setShowLightTheme] = useState<Boolean>(false)
+
+  return (
+    <div className="fixed right-[4rem] top-[4rem] cursor-pointer w-[24px] h-[24px] items-center flex">
+      <div
+        className="absolute left-0"
+        onClick={() => {
+          setShowLightTheme(!showLightTheme)
+        }}
+      >
+        {showLightTheme && <SunFilledIcon />}
+        {!showLightTheme && <SunOutlinedIcon />}
+      </div>
+    </div>
+  )
+}
