a120cbbc1b776107f0d232f3a34ea5df5f3d3905
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-28 19:57:50 +0530

    responsive correction for app layout for clean ui

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -4,7 +4,7 @@ type TAppLayoutProps = {
 
 export default function AppLayout(props: TAppLayoutProps) {
   return (
-    <main className="w-full h-[100%] m-auto flex flex-col w-4xl max-w-4xl">
+    <main className="w-full h-[100%] m-auto flex flex-col w-4xl max-w-4xl pt-20">
       {props.children}
     </main>
   )
