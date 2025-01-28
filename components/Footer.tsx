ce1d67dc263b05187270703c8c700589b015f9cd
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-28 22:54:30 +0530

    new info added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   const curDate = new Date()
   return (
-    <footer className="border-t py-2 text-center border-gray-800">
+    <footer className="border-t py-2 text-end border-gray-800 flex justify-between flex-row">
+      <p className="text-sm text-gray-700">
+        Tech Stacks: next.js v15 + react.js v19 + tailwindcss v4 + vercel
+      </p>
       <p className="text-gray-600 text-sm">
         © {curDate.getFullYear()} Cra2y Coder. All rights reserved.
       </p>
