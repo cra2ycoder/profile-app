a3ac0a5fc8026f4bc1c4184fb32ce1e905db0901
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-28 22:19:49 +0530

    just build enablement testing + profile image loading from sirv

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-// import Title from '@/components/Title'
-// import SideNav from '@/components/SideNav'
-// import AppLayout from '@/components/AppLayout'
-// import Footer from '@/components/Footer'
-// import ThemeToggler from '@/components/ThemeToggler'
-// import SocialAccounts from '@/components/SocialAccounts'
+import Title from '@/components/Title'
+import SideNav from '@/components/SideNav'
+import AppLayout from '@/components/AppLayout'
+import Footer from '@/components/Footer'
+import ThemeToggler from '@/components/ThemeToggler'
+import SocialAccounts from '@/components/SocialAccounts'
 // import SkillList from '@/components/SkillList'
-// import DownloadCV from '@/components/DownloadCV'
+import DownloadCV from '@/components/DownloadCV'
 // import UserInfo from '@/components/UserInfo'
 // import WorkExperience from '@/components/WorkExperience'
 // import Awards from '@/components/Awards'
@@ -14,32 +14,31 @@
 export default function Index() {
   return (
     <>
-      <div>home page</div>
-      {/* <SideNav />
+      <SideNav />
       <ThemeToggler />
       <AppLayout>
         <div className="w-full flex flex-col mb-10">
-          <UserInfo />
+          {/* <UserInfo /> */}
           <div className="flex flex-row gap-4 items-center mt-10">
             <DownloadCV />
             <div className="border-gray-600 border-r h-full" />
             <SocialAccounts />
           </div>
         </div>
-        <WorkExperience />
-        <SkillList
+        {/* <WorkExperience /> */}
+        {/* <SkillList
           filterBy={[
             { title: 'Technical Stacks', filterKey: 'tech' },
             { title: 'Design Tools', filterKey: 'design' },
           ]}
         />
-        <Awards />
+        <Awards /> */}
         <div className="w-full flex gap-4 flex-col mb-10">
           <Title text="What I Enjoy in My Free Time" />
         </div>
       </AppLayout>
-      <ArtWorks />
-      <Footer /> */}
+      {/* <ArtWorks /> */}
+      <Footer />
     </>
   )
 }
