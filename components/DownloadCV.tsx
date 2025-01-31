3e2b678a32478299d9f5104f279dce37e7739179
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-31 19:04:17 +0530

    deployment error and accessibility error resolved

--- components/DownloadCV.tsx
+++ components/DownloadCV.tsx
@@ -3,7 +3,7 @@ import { parseCSS } from '@/utils'
 export default function DownloadCV() {
   const themeClass = {
     default:
-      'w-auto rounded-full bg-gradient-to-r px-8 py-2 font-bold drop-shadow-md',
+      'w-auto rounded-full bg-gradient-to-r px-12 py-2 font-bold drop-shadow-md text-xl',
     light: 'from-gray-700 to-gray-950',
     dark: 'dark:from-blue-800 dark:to-purple-600',
   }
@@ -14,7 +14,7 @@ export default function DownloadCV() {
       href="https://cra2ycoder.sirv.com/mohanraj_profile.pdf"
       download
     >
-      <button className="text-lg px-3">Download CV</button>
+      <button>Download CV</button>
     </a>
   )
 }
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -28,6 +28,16 @@ export default async function WorkExperience() {
       <Title text="Companies I’ve Worked With" />
       <div className="flex flex-row gap-10 w-[100%] mt-6 flex-wrap">
         {apiResponse.data?.map((company: TWorkItem, index: number) => {
+          const {
+            name,
+            logo,
+            styles,
+            role,
+            duration,
+            responsibilities = [],
+            clients = [],
+          } = company
+
           return (
             <div
               className="flex flex-row pb-10 w-full flex-wrap border-b dark:border-gray-800 border-gray-300"
@@ -36,46 +46,44 @@ export default async function WorkExperience() {
               <div className="flex flex-row gap-2 items-center">
                 <div className="flex flex-row w-20 h-20 mr-2 flex-wrap drop-shadow-md bg-white rounded-full p-3 justify-center items-center overflow-hidden">
                   <Image
-                    src={company.logo}
-                    alt={`company-logo-${company.name}`}
+                    src={logo}
+                    alt={`company-logo-${name}`}
                     className="overflow-hidden"
-                    style={company.styles || {}}
+                    style={styles || {}}
                     width={80}
                     height={80}
                   />
                 </div>
                 <div className="flex flex-col gap-2">
                   <h3 className="dark:text-gray-200 text-gray-900 font-bold text-2xl font-[DM Sans]">
-                    {company.name}
+                    {name}
                   </h3>
                   <p className="text-gray-500">
-                    {company.role} | {company.duration}
+                    {role} | {duration}
                   </p>
                 </div>
               </div>
               <ol className="flex gap-3 flex-col mt-6 list-none max-w-3xl">
-                {company.responsibilities?.map(
-                  (resp: string, index: number) => (
-                    <li
-                      key={`resp-${index}`}
-                      className="font-[Inter] text-md dark:text-gray-400 text-gray-600"
-                      dangerouslySetInnerHTML={{ __html: resp }}
-                    />
-                  )
-                )}
+                {responsibilities?.map((resp: string, index: number) => (
+                  <li
+                    key={`resp-${index}`}
+                    className="font-[Inter] text-md dark:text-gray-400 text-gray-600"
+                    dangerouslySetInnerHTML={{ __html: resp }}
+                  />
+                ))}
               </ol>
-              {company.clients && (
-                <ol className="flex gap-3 flex-row mt-6 list-none max-w-3xl items-center">
-                  {company.clients?.map((client: string, index: number) => (
+              {clients && (
+                <ol className="flex flex-row gap-3 mt-6 list-none max-w-3xl items-center">
+                  {clients?.map((client: string, index: number) => (
                     <Fragment key={`client-${index}`}>
-                      <li className="dark:text-gray-300 text-gray-400 font-extrabold text-[0.9rem] py-2">
+                      <li className="dark:text-gray-300 text-gray-400 font-extrabold text-[0.9rem]">
                         {client}
+                        {index < clients.length - 1 && (
+                          <span className="text-sm dark:text-gray-100 text-gray-400 ml-3">
+                            |
+                          </span>
+                        )}
                       </li>
-                      {index < company.clients?.length - 1 && (
-                        <div className="dark:text-gray-800 text-gray-400">
-                          |
-                        </div>
-                      )}
                     </Fragment>
                   ))}
                 </ol>
