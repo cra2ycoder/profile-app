a059b615ce6feef3842b9cf443113498779203f4
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-02-01 22:40:09 +0530

    minor ui fixes

--- components/DownloadCV.tsx
+++ components/DownloadCV.tsx
@@ -4,7 +4,7 @@ import { parseCSS } from '@/utils'
 export default function DownloadCV() {
   const themeClass = {
     default:
-      'w-full rounded-full bg-gradient-to-r px-12 py-2 font-bold drop-shadow-md text-xl block flex justify-center',
+      'w-full rounded-full bg-gradient-to-r px-12 py-2 font-bold drop-shadow-md text-xl block flex justify-center text-white',
     light: 'from-gray-700 to-gray-950',
     dark: 'dark:from-blue-800 dark:to-purple-600',
   }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   const curDate = new Date()
   return (
-    <footer className="border-t py-2 text-end dark:border-gray-800 border-gray-300 flex md:justify-between flex-row flex-wrap md:flex-nowrap justify-center">
+    <footer className="border-t py-2 gap-3 dark:border-gray-800 border-gray-300 flex md:justify-between flex-row flex-wrap md:flex-nowrap justify-center">
       <p className="text-sm dark:text-gray-700 text-gray-500">
         Tech Stacks: Next.js + React.js + Tailwindcss + Vercel
       </p>
