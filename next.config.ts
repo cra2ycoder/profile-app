de266d6567608809f22faa5d34b8e4dcd7f7b6e3
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-12-12 07:27:13 +0530

    image loading issue fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     domains: ['localhost', 'cra2ycoder.sirv.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cra2ycoder.sirv.com',
+      },
+    ],
   },
 }
 
