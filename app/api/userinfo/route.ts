a90a87052d36a270ad4977c583551c9848e3335e
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-12-12 07:17:22 +0530

    new changes

--- app/api/userinfo/route.ts
+++ app/api/userinfo/route.ts
@@ -9,11 +9,11 @@ export async function GET() {
       data: {
         name: 'Mohanraj Rangasamy',
         role: [
-          'Frontend Developer',
-          'JS / TS, Node.js, Next.js, React.js, React Query, Cypress',
+          'Fullstack Developer',
+          'JS / TS, Node.js, SQL, Fastify, Next.js, React.js, React Query, Jest, Cypress',
           'MFE',
         ],
-        description: `Frontend Developer & UI/UX Designer with <hlt>16 years of experience (8 years in each)</hlt>, specializing in responsive, user-friendly, and <hlt>microfrontend-based web applications</hlt>. Proficient in <hlt>JavaScript, TypeScript, React.js, Next.js, Node.js, and TanStack React Query</hlt>, with expertise in performance, accessibility, and seamless UX. Skilled in <hlt>Figma, Sketch, Adobe Photoshop, and XD</hlt>, bridging design and development. Passionate about clean code, optimization, and staying ahead of web trends.`,
+        description: `Fullstack Developer & UI/UX Designer with <hlt>16 years of experience (8 years in each)</hlt>, specializing in responsive, user-friendly, and <hlt>microfrontend-based web applications</hlt>. Proficient in <hlt>JavaScript, TypeScript, React.js, SQL, Fastify, Next.js, Node.js, Jest and TanStack React Query</hlt>, with expertise in performance, accessibility, and seamless UX. Skilled in <hlt>Figma, Sketch, Adobe Photoshop, and XD</hlt>, bridging design and development. Passionate about clean code, optimization, and staying ahead of web trends.`,
         phoneNumber: '+(91) 770 829 629 5',
         email: 'mohanraj.dev.6789@gmail.com',
         location: 'Bangalore, Karnataka',
